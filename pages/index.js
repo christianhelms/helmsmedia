ecee7de8886bc69bd0012b5e162e78078e73b050
christianhelms <helmsmedia@gmail.com>
2022-02-25 22:52:56 -0600

    home page setup with aos

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+//react
 import React from 'react';
+//css
+import './css/style.scss';
+import AOS from 'aos';
 
+//components
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
 import FeaturesHome from '../partials/FeaturesHome';
@@ -8,6 +13,25 @@ import FeaturesWorld from '../partials/FeaturesWorld';
 import News from '../partials/News';
 import Cta from '../partials/Cta';
 import Footer from '../partials/Footer';
+
+const location = useLocation();
+
+useEffect(() => {
+  AOS.init({
+    once: true,
+    disable: 'phone',
+    duration: 700,
+    easing: 'ease-out-cubic',
+  });
+  // eslint-disable-next-line no-unused-vars
+  const sticky = new Sticky('[data-sticky]');
+});
+
+useEffect(() => {
+  document.querySelector('html').style.scrollBehavior = 'auto'
+  window.scroll({ top: 0 })
+  document.querySelector('html').style.scrollBehavior = ''
+}, [location.pathname]); // triggered on route change
 
 export default function Home() {
   return (
